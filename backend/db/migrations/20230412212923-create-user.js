92db8d97d2e7a4529c4ac51e654ccb52802adc17
tienhoang <xuantien93@yahoo.com>
2023-04-13 18:37:48 -0400

    refactor

--- backend/db/migrations/20230412212923-create-user.js
+++ backend/db/migrations/20230412212923-create-user.js
@@ -22,6 +22,14 @@ module.exports = {
         allowNull: false,
         unique: true
       },
+      firstName: {
+        type: Sequelize.STRING(30),
+        allowNull: false
+      },
+      lastName: {
+        type: Sequelize.STRING(30),
+        allowNull: false
+      },
       email: {
         type: Sequelize.STRING(256),
         allowNull: false,
--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -46,6 +46,8 @@ router.post(
 
         const safeUser = {
             id: user.id,
+            firstName: user.firstName,
+            lastName: user.lastName,
             email: user.email,
             username: user.username,
         };
@@ -73,6 +75,8 @@ router.get(
         if (user) {
             const safeUser = {
                 id: user.id,
+                firstName: user.firstName,
+                lastName: user.lastName,
                 email: user.email,
                 username: user.username,
             };
