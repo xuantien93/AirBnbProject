ee69259ec410c78985614d0335c4519c8c673856
tienhoang <xuantien93@yahoo.com>
2023-05-05 17:46:56 -0400

    modal setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,7 @@ import App from './App';
 import configureStore from './store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
 import * as sessionActions from "./store/session";
+import { Modal, ModalProvider } from './context/Modal';
 
 const store = configureStore()
 
@@ -20,11 +21,14 @@ if (process.env.NODE_ENV !== "production") {
 
 function Root() {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <ModalProvider>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+          <Modal />
+        </BrowserRouter>
+      </Provider>
+    </ModalProvider>
   );
 }
 
