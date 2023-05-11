b86c1e5cb77c49d30a470fbbcb57d3dc9fd85092
tienhoang <xuantien93@yahoo.com>
2023-05-11 03:03:58 -0400

    time

--- frontend/src/components/SpotBySpotId/SpotReviews.js
+++ frontend/src/components/SpotBySpotId/SpotReviews.js
@@ -21,26 +21,22 @@ const SpotReviews = ({ reviews, spotId }) => {
         dispatch(getAllReviewsBySpotIdThunk(spotId))
     }, [dispatch, spotId])
 
-    // console.log("this is user", user)
-    // console.log("this is spot", spot)
-    // console.log("this is review", reviews)
-
     if (!spot || !spotId || !user) return null
 
-    let months = {
-        "01": "January",
-        "02": "Feburary",
-        "03": "March",
-        "04": "April",
-        "05": "May",
-        "06": "June",
-        "07": "July",
-        "08": "August",
-        "09": "September",
-        "10": "October",
-        "11": "November",
-        "12": "December"
-    }
+    // let months = {
+    //     "01": "January",
+    //     "02": "Feburary",
+    //     "03": "March",
+    //     "04": "April",
+    //     "05": "May",
+    //     "06": "June",
+    //     "07": "July",
+    //     "08": "August",
+    //     "09": "September",
+    //     "10": "October",
+    //     "11": "November",
+    //     "12": "December"
+    // }
 
     const newReviews = reviews.toReversed()
 
@@ -64,7 +60,8 @@ const SpotReviews = ({ reviews, spotId }) => {
                                 <div key={review.id}>
                                     <div className="review-detail">
                                         <h3>{review.User.firstName} {review.User.lastName}</h3>
-                                        <p className="review-date">{months[review.createdAt.slice(5, 7)]} {review.createdAt.slice(0, 4)}</p>
+                                        {/* <p className="review-date">{months[review.createdAt.slice(5, 7)]} {review.createdAt.slice(0, 4)}</p> */}
+                                        <p className="review-date">{new Date(review.createdAt).toLocaleDateString()}</p>
                                         <p>{review.review}</p>
                                         {(review.userId === user?.id) &&
                                             <div>
